87eaf658ec137b22fb2249d05ab58a61dff24eda
FreXlua <156111597+FreXlua@users.noreply.github.com>
2024-03-16 16:24:18 +0400

    Add files via upload

--- autoupdate.lua
+++ autoupdate.lua
@@ -9,13 +9,13 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 1
-local script_vers_text = '1.00'
+local script_vers = 2
+local script_vers_text = '2.00'
 
 local update_url = 'https://raw.githubusercontent.com/FreXlua/test/main/update.ini'
 local update_path = getWorkingDirectory() .. '/update.ini'
 
-local script_url = ''
+local script_url = 'https://raw.githubusercontent.com/FreXlua/test/main/autoupdate.lua'
 local script_path = thisScript().path
 
 function main()
@@ -30,7 +30,7 @@ function main()
         if status == dlstatus.STATUS_ENDDOWNLOADDATA then
             updateIni = inicfg.load(nil, update_path)
             if tonumber(updateIni.info.vers) > script_vers then
-                sampAddChatMessage('Есть обнова! Пропиши /upd чтобы обновить скрипт', 0xff0000)
+                sampAddChatMessage('пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ! пїЅпїЅпїЅпїЅпїЅпїЅпїЅ /upd пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ', 0xff0000)
                 update_state = true
             end
             os.remove(update_path)
@@ -43,7 +43,7 @@ function main()
         if update_state then
             downloadUrlToFile(script_url, script_path, function(id, status)
                 if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-                    sampAddChatMessage('Скрипт успешно обновлен!', 0xff0000)
+                    sampAddChatMessage('пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!', 0xff0000)
                     thisScript():reload()
                 end
             end)
@@ -53,5 +53,5 @@ function main()
 end
 
 function cmd_upd(arg)
-    sampShowDialog(1000, 'Автообновление', 'by FreX script', 'Закрыть', '', 0)
+    sampShowDialog(1000, 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ', 'by FreX script', 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ', '', 0)
 end
